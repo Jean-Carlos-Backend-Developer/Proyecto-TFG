be31588ac3b1c25ed032ce775a6876a3e75d8625
Jean Carlos <99887413+Jean-Carlos-Backend-Developer@users.noreply.github.com>
2024-09-16 15:36:17 +0200

    Update DBConn.cs

--- OrdenaYaVersion1/Conexion/DBConn.cs
+++ OrdenaYaVersion1/Conexion/DBConn.cs
@@ -3,7 +3,7 @@ namespace OrdenaYaVersion1.Conexion
 {
 	public class DBConn
 	{
-		public const string WepApyAuthentication = "AIzaSyCHivhRiI8rXUODIjm2Wox-MB7UxKvHP4w";
+		public const string WepApyAuthentication = "clave-de-tu-web-apy";
 	}
 }
 
